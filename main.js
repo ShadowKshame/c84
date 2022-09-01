564de7c3e16a18ef9383ef05e9dbeb1ee0d590b8
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-09-01 09:46:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=10;
+rover_y=10;
+background_image="mars.jpg";
+rover_image="rover.png";
+function add() {
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
+
+   
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed  == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed  == '40')
+    {
+        down();
+        console.log("down");
+    }
+    if(keyPressed  == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keyPressed  == '39')
+    {
+        right();
+        console.log("right");
+    }
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
